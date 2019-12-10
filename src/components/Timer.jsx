ee773f2e3843afabd28cf368337d22a8b31b1559
OmoGr3y <anthonyomoijahe@gmail.com>
2019-12-10 11:17:33 +0100

    pomodoro-timer complete

---
+++ src/components/Timer.jsx
@@ -0,0 +1,117 @@
+import React, { Component } from "react";
+import "./Timer.css";
+
+class Timer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      alert: { type: "", message: "" },
+      time: 0
+    };
+
+    this.times = {
+      defaultTime: 1500,
+      shortBreak: 300,
+      longBreak: 900
+    };
+  }
+
+  componentDidMount() {
+    this.setDefaultTime();
+  }
+
+  setDefaultTime = () => {
+    this.setState({
+      time: this.times.defaultTime
+    });
+  };
+
+  setTime = newTime => {
+    this.restartInterval();
+
+    this.setState({
+      time: newTime
+    });
+  };
+
+  restartInterval = () => {
+    clearInterval(this.interval);
+    this.interval = setInterval(this.countDown, 1000);
+  };
+  countDown = () => {
+    if (this.state.time === 0) {
+      this.setState({
+        alert: {
+          type: "buzz",
+          message: "Buzzzzzzz!"
+        }
+      });
+    } else {
+      this.setState({
+        time: this.state.time - 1
+      });
+    }
+  };
+
+  setTimeWork = () => {
+    this.setState({
+      alert: {
+        type: "work",
+        message: "working!"
+      }
+    });
+    return this.setTime(this.times.defaultTime);
+  };
+
+  setTimeForShortBreak = () => {
+    this.setState({
+      alert: {
+        type: "shortBreak",
+        message: "Taking a short Break!"
+      }
+    });
+    return this.setTime(this.times.shortBreak);
+  };
+
+  setTimeForlongBreak = () => {
+    this.setState({
+      alert: {
+        type: "longBreak",
+        message: "Taking a Long Break!"
+      }
+    });
+    return this.setTime(this.times.longBreak);
+  };
+
+  displayTimer(seconds) {
+    const m = Math.floor((seconds % 3600) / 60);
+    const s = Math.floor((seconds % 3600) % 60);
+
+    return `${m < 10 ? "0" : ""}${m}: ${s < 10 ? "0" : ""}${s}`;
+  }
+  render() {
+    const {
+      alert: { message, type },
+      time
+    } = this.state;
+    return (
+      <div className="Pomodoro">
+        <div className={`alert ${type}`}>{message}</div>
+        <div className="timer">{this.displayTimer(time)}</div>
+        <div>
+          <button className="start" onClick={this.setTimeWork}>
+            Start Working
+          </button>
+          <button className="short" onClick={this.setTimeForShortBreak}>
+            Short Break
+          </button>
+          <button className="long" onClick={this.setTimeForlongBreak}>
+            Long Break
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Timer;

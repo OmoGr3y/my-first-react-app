97831f680d91c82d96eea038ee311ccf64f1dadf
OmoGr3y <anthonyomoijahe@gmail.com>
2019-12-10 14:44:51 +0100

    router complete

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import logo from "../shared/images/logo.svg";
 
 const Header = ({
@@ -7,11 +8,25 @@ const Header = ({
   url = "http://localhost:3000"
 }) => {
   return (
-    <header className="App">
+    <header className="App-header">
       <a href={url}>
         <img src={logo} className="App-logo" alt="logo" />
       </a>
       <h1 className="App-title">{title}</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/notes">Notes</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
     </header>
   );
 };
--- src/routes.jsx
+++ src/routes.jsx
@@ -6,13 +6,16 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error404 from "./components/Error404";
+import Notes from "./components/Notes";
 
 const AppRoutes = () => (
   <App>
     <Switch>
       <Route path="/" component={Home} exact />
       <Route path="/about" component={About} exact />
+      <Route path="/notes" component={Notes} exact />
       <Route path="/contact" component={Contact} exact />
+      <Route path="/notes/:noteId" component={Notes} exact />
       <Route component={Error404} />
     </Switch>
   </App>
